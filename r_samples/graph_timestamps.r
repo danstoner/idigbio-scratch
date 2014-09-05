19c0921c443d43e79f4cf14fb172a7f8e5d3d123
Dan Stoner <danstoner@gmail.com>
2014-09-05 14:37:23 -0400

    r scripted and chart output to png

--- r_samples/graph_timestamps.r
+++ r_samples/graph_timestamps.r
@@ -3,12 +3,18 @@
  
 
 # 
+png(filename="output_uploads_per_day.png",width=800)
 uploads <- read.csv('uploads20140902.csv', header=FALSE)
+
+# change margins
+par(mai=c(.75,.1,.5,.5))
 
 uploadstamps <- c(as.POSIXlt (strftime(uploads$V2)) )
 
-myhist <- hist(uploadstamps,breaks="days",main="Upload events per day",xlab="Date",ylab="",las=2,xaxt="n",yaxt="n",freq=TRUE)
+myhist <- hist(uploadstamps,breaks="days",main="Upload events per day",xlab="",ylab="",las=2,xaxt="n",yaxt="n",col="darkgrey",freq=TRUE)
 
 axis.POSIXct(1, at=seq((uploadstamps[1]-86400), as.POSIXlt(Sys.time()), by="day"), format="%b %d", srt=45, las=2,cex.axis=0.7)
 
 axis(4, at=myhist$freq)
+box()
+dev.off()
