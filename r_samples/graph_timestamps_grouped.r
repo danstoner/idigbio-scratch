84e9cca56346cb1dd2d32d076f30a5f843db34ba
Dan Stoner <danstoner@gmail.com>
2014-09-11 17:09:33 -0400

    give columns appropriate names. generate graph.  Still does not have zero count days in graph.

--- r_samples/graph_timestamps_grouped.r
+++ r_samples/graph_timestamps_grouped.r
@@ -1,13 +1,20 @@
 # Group the data in Postgresql SQL query to reduce the number of data points.
 # csv is generated via
 # 
-# COPY (select date_trunc('day',date_modified),count(*) from idb_object_keys WHERE  user_uuid = '285a4be0-5cfe-4d4f-9c8b-b0f0f3571079' group by date_trunc order by date_trunc)  to '/tmp/uploadsbyday20140909.csv' with CSV;
+# COPY (select date_trunc('day',date_modified),count(*) from idb_object_keys WHERE  user_uuid = '285a4be0-5cfe-4d4f-9c8b-b0f0f3571079' group by date_trunc order by date_trunc)  to '/tmp/uploadsbyday20140911.csv' with CSV;
 
 # 
 png(filename="output_uploads_per_day_grouped.png",width=800)
-uploads <- read.csv('uploadsbyday20140909.csv', header=FALSE)
+uploads <- read.csv('uploadsbyday20140911.csv', header=FALSE)
+colnames(uploads) <- c("date","uploadcount")
 
-barplot(uploads$V2)
+#plot.ts(uploads$uploadcount)
+
+barplot(uploads$uploadcount)
+
+
+
+
 
 #### from other example
 # change margins
