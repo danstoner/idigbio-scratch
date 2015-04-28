f49ab5b0d8368ab7156135a4565c67b42218b0c9
Dan Stoner <danstoner@gmail.com>
2015-04-28 16:33:18 -0400

    created

---
+++ api_examples/puma_concolor_using_idigbio_library.py
@@ -0,0 +1,33 @@
+import idigbio
+
+query_as_string = '{"rq":{"scientificname":"puma concolor"}}'
+
+api = idigbio.json()
+
+record_list = api.search_records(query_as_string)
+
+for item in record_list["items"]:
+      for key in item["indexTerms"]:
+          if key == "verbatimlocality":
+              print item["indexTerms"]["verbatimlocality"]
+
+
+
+#print type(record_list)
+
+
+# query_json = json.loads(query_as_string)
+
+
+
+
+# r = requests.post('http://beta-search.idigbio.org/v2/search/records/',data=json.dumps(query_json), headers={'content-type': 'application/json'})
+
+# response = r.json()
+
+# for item in response["items"]:
+# # not all items will have all fields
+#      for key in item["indexTerms"]:
+#          if key == "verbatimlocality":
+#              print item["indexTerms"]["verbatimlocality"]
+
