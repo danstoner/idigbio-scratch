e1dc136e86835f5af7ef3f006e086278f4be58af
Dan Stoner <danstoner@gmail.com>
2015-04-28 16:38:25 -0400

    same features now

--- api_examples/puma_concolor_using_api_direct.py
+++ api_examples/puma_concolor_using_api_direct.py
@@ -3,14 +3,11 @@ import json
 
 query_as_string = '{"rq":{"scientificname":"puma concolor"}}'
 
-query_json = json.loads(query_as_string)
-
-r = requests.post('http://beta-search.idigbio.org/v2/search/records/',data=json.dumps(query_json), headers={'content-type': 'application/json'})
+r = requests.post('http://beta-search.idigbio.org/v2/search/records/',data=query_as_string, headers={'content-type': 'application/json'})
 
 response = r.json()
 
 for item in response["items"]:
-# not all items will have all fields
      for key in item["indexTerms"]:
          if key == "verbatimlocality":
              print item["indexTerms"]["verbatimlocality"]
--- api_examples/puma_concolor_using_idigbio_library.py
+++ api_examples/puma_concolor_using_idigbio_library.py
@@ -12,22 +12,3 @@ for item in record_list["items"]:
               print item["indexTerms"]["verbatimlocality"]
 
 
-
-#print type(record_list)
-
-
-# query_json = json.loads(query_as_string)
-
-
-
-
-# r = requests.post('http://beta-search.idigbio.org/v2/search/records/',data=json.dumps(query_json), headers={'content-type': 'application/json'})
-
-# response = r.json()
-
-# for item in response["items"]:
-# # not all items will have all fields
-#      for key in item["indexTerms"]:
-#          if key == "verbatimlocality":
-#              print item["indexTerms"]["verbatimlocality"]
-
