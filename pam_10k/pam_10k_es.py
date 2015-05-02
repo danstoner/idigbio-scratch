cf595814386b17aa3d8adcac7edd198d689e040c
Dan Stoner <danstoner@gmail.com>
2015-05-02 10:54:26 -0400

    can generate a CSV. still need to extract lat long into their own columns

--- pam_10k/pam_10k_es.py
+++ pam_10k/pam_10k_es.py
@@ -1,12 +1,26 @@
-import argparse
-#import idigbio
-import json
-import requests
+"""
+This script uses a hybrid of iDigBio search api and direct Elasticsearch queries.
 
-# rewrite to talk to ES directly
+Generates a CSV for a query that uses a large number of distinct input values on a single field (e.g. 16k genera)
+"""
+try:
+    import argparse
+    import idigbio
+    import json
+    import requests
+    import csv
+except ImportError, e:
+    print "IMPORT ERROR: %s" % e
+    raise SystemExit
+
+## output specification:
+# Genus, specific epithet, lat, long, locality (all fields so that I could
+# have country, state, county, etc. so that we can check the lat/long
+# against the field)
 
 parser = argparse.ArgumentParser(description='Read list of values from file and search for records matching in specified field.')
 parser.add_argument('-i', '--inputfile', dest='inputfile', required=True, help="Input file must be one value per line.")
+parser.add_argument('-o', '--outputfile', dest='outputfile', required=True, help="Output filename for results (something.csv).")
 parser.add_argument('-f', '--field', dest='field', default='scientificname', help="The specified field must be an iDigbio indexed term.")
 parser.add_argument('--header-row', dest='header_row', default=False, action='store_true', help="Use this option if the first line of the input file is a header row.")
 parser.add_argument('--stop-count', dest='stopcount', type=int, help="Stop reading inputfile after this many rows. Default: 10")
@@ -14,7 +28,8 @@ args = parser.parse_args()
 
 
 inputfile = args.inputfile
-field = args.field
+outputfile = args.outputfile
+searchfield = args.field
 header_needs_skipped = args.header_row
 if args.stopcount:
 # may have issues if stopcount > 7000
@@ -22,19 +37,43 @@ if args.stopcount:
 else:
     stopcount = 10
 
+# These are output fields, same as the fields we pull out of the search results
+fields = ["uuid", "genus", "specificepithet", "geopoint", "country", "stateprovince", "county", "municipality"]
+
+# If we cannot open output file for write, might as well stop now.
+with open(outputfile,"w") as f:
+    writer = csv.writer(f)
+    writer.writerow(fields)
+
+
+
+# inputset will hold the lines we actually want to work on
 inputset = set()
+# zerorecordset will hold the lines that don't have any matching records
+zerorecordsset = set()
 
 count = 0
+
+def getmatchingcount(apiobj,thingtocount):  
+    num = api.count_records(rq={searchfield: thingtocount})
+    return num
+
+api = idigbio.json()
 
 with open(inputfile, 'r') as f:
     for line in f:
         if header_needs_skipped:
             header_needs_skipped = False
         else:
-            inputset.add(line.strip())
+            if getmatchingcount(api,line.strip()) > 0:
+                inputset.add(line.strip())
+            else:
+                zerorecordsset.add(line.strip())
             count += 1
         if count >= stopcount:
             break
+
+# print zerorecordsset
 
 # {
 #    "filter" : {
@@ -48,12 +87,18 @@ with open(inputfile, 'r') as f:
 # }
 
 
+
+ 
+# answer will hold the items that have geopoint
 answer = dict()
-fields = ["uuid", "genus", "specificepithet", "geopoint", "country", "stateprovince", "county", "municipality"]
+# answer_nogeopoint will hold the items that were thrown out because they lack geopoint
+answer_nogeopoint = dict()
+
+
 
 query = { "filter" : {
         "terms" : {
-        "genus" : []
+        searchfield : []
 }}}
 
 #query = {}
@@ -62,7 +107,7 @@ query = { "filter" : {
 
 
 for each in inputset:
-    query["filter"]["terms"]["genus"].append(each.lower())
+    query["filter"]["terms"][searchfield].append(each.lower())
 
 print query
 query_json = json.dumps(query)
@@ -97,11 +142,22 @@ for hit in response_json["hits"]["hits"]:
                 answer[id].append(hit["_source"][field])
             else:
                 answer[id].append("")
-        print answer[id]
+#        print answer[id]
 #    print hit
 
 
+
+print "Number of records for output: ", len(answer)
+# write the data to csv     
+with open(outputfile,"a") as f:
+    writer = csv.writer(f)
+    for a in answer:
+        writer.writerow(answer[a])
+
 raise SystemExit
+
+
+### __END__
 
 while len(inputset) > 0:
     while (place < 100) and len(inputset) > 0:
@@ -142,25 +198,10 @@ while len(inputset) > 0:
 
 
 
-
 raise SystemExit
 
-api = idigbio.json()
 
-#record_list = api.search_records(rq={"genus":["abelia","abelmoschus"]})
-record_list = api.search_records(rq=query)
-
-## output specification:
-# Genus, specific epithet, lat, long, locality (all fields so that I could
-# have country, state, county, etc. so that we can check the lat/long
-# against the field)
-
-#print record_list["itemCount"]
-
-
-raise SystemExit
-
-for item in record_list["items"]:
-    for key in item["indexTerms"]:
-        if key == "genus":
-            print item["indexTerms"][key]
+# for item in record_list["items"]:
+#     for key in item["indexTerms"]:
+#         if key == "genus":
+#             print item["indexTerms"][key]
